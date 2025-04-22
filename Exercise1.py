dc9ecaaf9c556c57a226da043fe446977c0db0f1
Juan Sebastian Peñuela Duran <jupenuelad@unal.edu.co>
2025-04-22 15:58:51 -0500

    Se cambió el idioma

--- Exercise1.py
+++ Exercise1.py
@@ -1,30 +1,30 @@
-#This function operate two numbers according to a chosen option
-def calculator(a, b, op):
+#Esta función opera dos números según una opción elegida
+def calculadora(a, b, op):
     if(op==1):
         return a+b
     elif(op==2):
         return a-b
     elif(op==3):
         return a*b
-    return a/b if(b!=0) else("Error. cannot be divided by zero")
+    return a/b if(b!=0) else("Error. No se puede dividir entre cero")
 
-#Options of menu
-op_1:str = "1.addition"
-op_2:str = "2.subtraction"
-op_3:str = "3.multiplication"
-op_4:str = "4.division"
+#Opciones de menú
+op_1:str = "1.suma"
+op_2:str = "2.resta"
+op_3:str = "3.multiplicación"
+op_4:str = "4.división"
 
-#This function validates that the chosen option is within the range 
-def validation_options():
+#Esta función valida que la opción elegida esté dentro del rango
+def validacion_opciones():
     print(f"{op_1}\n{op_2}\n{op_3}\n{op_4}")
-    op:int = int(input("Enter your option: "))
+    op:int = int(input("Ingrese su opción: "))
     while(op<1 or op>4):
         print(f"{op_1}\n{op_2}\n{op_3}\n{op_4}")
-        op:int = int(input("Error. Option out of range. Enter ypur option: "))
+        op:int = int(input("Error. Opción fuera de rango. Ingrese su opción: "))
     return op
 
-option:int = validation_options()
-num1:int = int(input("Enter a number: "))
-num2:int = int(input("Enter another number: "))
+opcion:int = validacion_opciones()
+num1:int = int(input("Ingrese un número: "))
+num2:int = int(input("Ingrese otro número: "))
 
-print(calculator(num1, num2, option))
+print(calculadora(num1, num2, opcion))
